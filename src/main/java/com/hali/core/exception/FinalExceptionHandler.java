0d239d805945417f95d7fa71c37024304f30573d
zzs <zzs@17666545369>
2019-05-22 10:14:20 +0800

    添加依赖 添加异常处理转换Controller

---
+++ src/main/java/com/hali/core/exception/FinalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.hali.core.exception;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hali.core.util.JsonDto;
+
+import cn.hutool.core.date.DateUtil;
+
+/**
+ * 
+* @F: com.hali.core.exception.FinalExceptionHandler.java
+* @ClassName: FinalExceptionHandler 
+* @Description: TODO(将错误页面返回解析为Code) 
+* @author ZZS
+* @date 2019年5月22日 上午9:38:55 
+*
+ */
+@RestController
+public class FinalExceptionHandler  implements ErrorController{
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return  "/error";
+	}
+  @RequestMapping(value="/eeor")
+	public JsonDto error(HttpServletResponse res,HttpServletRequest req) {
+	  JsonDto jd = new JsonDto();
+	  jd.setCode(String.valueOf(res.getStatus()));
+	  jd.setTime(DateUtil.formatDateTime(new Date()));  //401 重新登录
+	  if(res.getStatus() == org.springframework.http.HttpStatus.UNAUTHORIZED.value()) 
+		  jd.setMessage("请登录后重试");  //403无权限
+	  if(res.getStatus() == HttpStatus.FORBIDDEN.value()) 
+	     jd.setMessage("暂无权限访问该页面"); //404 页面未找到
+	  if(res.getStatus() == HttpStatus.NOT_FOUND.value())
+		  jd.setMessage("页面未找到");//500 内部错误
+	  if(res.getStatus() == HttpStatus.INTERNAL_SERVER_ERROR.value())
+		  jd.setMessage("接口或内部服务器错误");
+	  return jd;
+  }
+	
+}
---
+++ src/main/java/com/hali/core/util/JsonDto.java
@@ -0,0 +1,134 @@
+package com.hali.core.util;
+
+/**
+ * 
+* @F: com.hali.core.util.JsonDto.java
+* @ClassName: JsonDto 
+* @Description: TODO(Json接口传输类) 
+* @author ZZS
+* @date 2019年5月22日 上午9:57:54 
+*
+ */
+public class JsonDto {
+	public static final String JSON_SUCCESS = "1";
+	public static final String JSON_FAIL = "0";
+	public static final String JSON_ERROE = "-1";
+	public static final String JSON_FORBIDDEN = "403";
+	public static final String JSON_FORBIDDEN_MSG = "权限不够";
+	public static final String JSON_SUCCESS_MSG = "成功";
+	public static final String JSON_FAIL_MSG = "失败";
+	public static final String JSON_FAIL_NODATA = "查询无数据";
+	public static final String JSON_FAIL_AUTHCODE = "验证码错误或已失效";
+	public static final String JSON_FAIL_USEREXIST = "该用户名或手机已注册";
+	public static final String JSON_FAIL_USERNOEXIST = "用户名不存在";
+	public static final String JSON_FAIL_PWDERROR = "密码不正确";
+	public static final String JSON_ERROE_MSG = "错误";
+	private String code; // json返回码
+	private String message; // json返回信息
+	private Object data; // 返回数据
+
+	private String time;// 时间
+
+	/**
+	 * qiuhao:成功返回
+	 *
+	 * @date 2018年8月9日 下午5:19:57
+	 * @param @param msg
+	 * @param @return
+	 * @return JsonDto
+	 */
+	public JsonDto ok(Object msg) {
+		JsonDto r = new JsonDto(JsonDto.JSON_SUCCESS, JsonDto.JSON_SUCCESS_MSG, msg);
+		return r;
+	}
+
+	/**
+	 * qiuhao:成功返回
+	 *
+	 * @date 2018年8月9日 下午5:19:57
+	 * @param @param msg
+	 * @param @return
+	 * @return JsonDto
+	 */
+	public JsonDto ok() {
+		JsonDto r = new JsonDto(JsonDto.JSON_SUCCESS, JsonDto.JSON_SUCCESS_MSG);
+		return r;
+	}
+
+	/**
+	 * qiuhao:错误返回
+	 *
+	 * @date 2018年8月9日 下午5:19:57
+	 * @param @param msg
+	 * @param @return
+	 * @return JsonDto
+	 */
+	public JsonDto error(Object msg) {
+		JsonDto r = new JsonDto(JsonDto.JSON_ERROE, JsonDto.JSON_ERROE_MSG, msg);
+		return r;
+	}
+
+	/**
+	 * qiuhao:错误返回
+	 *
+	 * @date 2018年8月9日 下午5:19:57
+	 * @param @param msg
+	 * @param @return
+	 * @return JsonDto
+	 */
+	public JsonDto error() {
+		JsonDto r = new JsonDto(JsonDto.JSON_ERROE, JsonDto.JSON_ERROE_MSG);
+		return r;
+	}
+
+	public JsonDto() {
+		super();
+	}
+
+	public JsonDto(String code, String message) {
+		super();
+		this.code = code;
+		this.message = message;
+	}
+
+	public JsonDto(String code, String message, Object data) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
+
