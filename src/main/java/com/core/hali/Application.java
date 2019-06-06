54fb9f290d8eb8a195c6d629ba8114e336834f9f
zhouzhisheng <17666545369@163.com>
2019-06-07 00:20:33 +0800

    修改springboot 启动类

--- src/main/java/com/core/hali/Application.java
+++ src/main/java/com/core/hali/Application.java
@@ -1,6 +1,25 @@
 package com.core.hali;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author zhisheng.zhou
+ *@data: 2019/06/07
+ *
+ */
+@Slf4j
+@SpringBootApplication
+@EnableCaching
+@EnableJpaAuditing
+@EnableJpaRepositories()
 public class Application {
-
+public static void main(String[] args) {
+	
+}
 }
