f514fc3cb3ff00a569aa5dd91b6183be941f8c9b
zhouzhisheng <17666545369@163.com>
2019-06-08 11:02:32 +0800

    启动类

--- src/main/java/com/core/hali/Application.java
+++ src/main/java/com/core/hali/Application.java
@@ -1,10 +1,13 @@
 package com.core.hali;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import cn.hutool.log.Log;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -20,6 +23,8 @@ import lombok.extern.slf4j.Slf4j;
 @EnableJpaRepositories()
 public class Application {
 public static void main(String[] args) {
-	
+	ApplicationContext context = SpringApplication.run(Application.class, args);
+	String serverPort = context.getEnvironment().getProperty("server.port");
+	log.info("Hali started at http://localhost: "+ serverPort);
 }
 }
