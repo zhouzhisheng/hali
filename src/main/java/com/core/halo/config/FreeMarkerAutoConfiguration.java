f0084e743c70f2d913ce82cccb94b0c68a7cb4ef
zhouzhisheng <17666545369@163.com>
2019-06-08 11:07:29 +0800

    Freemaker 编辑器

---
+++ src/main/java/com/core/halo/config/FreeMarkerAutoConfiguration.java
@@ -0,0 +1,26 @@
+package com.core.halo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <pre>
+ *  FreeMaker 配置 Freemaker 编辑器 
+ * </pre>
+ * @author zhisheng.zhou
+ * @date: 2019/06/08
+ */
+@Slf4j
+@Configuration
+public class FreeMarkerAutoConfiguration {
+
+	@Autowired
+	private  freemarker.template.Configuration configuration;
+	
+	
+	
+	
+}
