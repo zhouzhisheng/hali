5bd5915434a978ec1b1506123d051cdce0cee065
zhouzhisheng <17666545369@163.com>
2019-06-12 22:44:03 +0800

    添加

--- src/main/java/com/core/halo/config/FreeMarkerAutoConfiguration.java
+++ src/main/java/com/core/halo/config/FreeMarkerAutoConfiguration.java
@@ -4,6 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 
+import cc.ryanc.halo.model.freemarker.method.RandomMethod;
+import cc.ryanc.halo.model.freemarker.method.RecentCommentsMethod;
+import cc.ryanc.halo.model.freemarker.method.RecentPostsMethod;
+import cc.ryanc.halo.model.freemarker.tag.ArticleTagDirective;
+import cc.ryanc.halo.model.freemarker.tag.CommonTagDirective;
+import cc.ryanc.halo.service.OptionsService;
+import cc.ryanc.halo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -20,9 +27,23 @@ public class FreeMarkerAutoConfiguration {
 	@Autowired
 	private  freemarker.template.Configuration configuration;
 	
-	@Autowired
-	  private OPtionsSer
-   @Autowired
-	private 
+    @Autowired
+    private OptionsService optionsService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private CommonTagDirective commonTagDirective;
+    @Autowired
+    private ArticleTagDirective articleTagDirective;
+    @Autowired
+    private RandomMethod randomMethod;
+
+    @Autowired
+    private RecentPostsMethod recentPostsMethod;
+
+    @Autowired
+    private RecentCommentsMethod recentCommentsMethod;
+    
+    
 	
 }
