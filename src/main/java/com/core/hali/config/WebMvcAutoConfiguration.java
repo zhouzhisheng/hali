ef0e4cfdc393159b2098683e8c246394e65cbf17
zhouzhisheng <17666545369@163.com>
2019-06-14 20:27:16 +0800

    添加

--- src/main/java/com/core/hali/config/WebMvcAutoConfiguration.java
+++ src/main/java/com/core/hali/config/WebMvcAutoConfiguration.java
@@ -1,10 +1,15 @@
 package com.core.hali.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import cc.ryanc.halo.web.interceptor.ApiInterceptor;
+import cc.ryanc.halo.web.interceptor.InstallInterceptor;
+import cc.ryanc.halo.web.interceptor.LocaleInterceptor;
+import cc.ryanc.halo.web.interceptor.LoginInterceptor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -20,5 +25,15 @@ import lombok.extern.slf4j.Slf4j;
 @ComponentScan(basePackages = "com.core.hali.web.controller")
 @PropertySource(value = "classpath:application.yml" )
 public class WebMvcAutoConfiguration {
+	@Autowired
+    private LoginInterceptor loginInterceptor;
 
+    @Autowired
+    private InstallInterceptor installInterceptor;
+
+    @Autowired
+    private ApiInterceptor apiInterceptor;
+
+    @Autowired
+    private LocaleInterceptor localeInterceptor;
 }
