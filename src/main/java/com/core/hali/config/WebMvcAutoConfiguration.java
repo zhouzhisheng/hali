08420caf76aa32437366f10520c0c14ec1d33ece
zhouzhisheng <17666545369@163.com>
2019-06-14 20:27:45 +0800

    修改

--- src/main/java/com/core/hali/config/WebMvcAutoConfiguration.java
+++ src/main/java/com/core/hali/config/WebMvcAutoConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import cc.ryanc.halo.web.interceptor.ApiInterceptor;
 import cc.ryanc.halo.web.interceptor.InstallInterceptor;
@@ -36,4 +38,53 @@ public class WebMvcAutoConfiguration {
 
     @Autowired
     private LocaleInterceptor localeInterceptor;
+    
+    
+    /**
+     * 注册拦截器
+     *
+     * @param registry registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/admin.*")
+                .addPathPatterns("/admin/**")
+                .addPathPatterns("/backup/**")
+                .excludePathPatterns("/admin/login")
+                .excludePathPatterns("/admin/getLogin")
+                .excludePathPatterns("/static/**");
+        registry.addInterceptor(installInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/install")
+                .excludePathPatterns("/install/do")
+                .excludePathPatterns("/static/**");
+        registry.addInterceptor(apiInterceptor)
+                .addPathPatterns("/api/**");
+        registry.addInterceptor(localeInterceptor)
+                .addPathPatterns("/admin.*")
+                .addPathPatterns("/admin/**")
+                .addPathPatterns("/install");
+        registry.addInterceptor(localeChangeInterceptor())
+                .addPathPatterns("/install");
+    }
+
+    /**
+     * 配置静态资源路径
+     *
+     * @param registry registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/themes/");
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:///" + System.getProperties().getProperty("user.home") + "/halo/upload/");
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/static/halo-backend/images/favicon.ico");
+        registry.addResourceHandler("/backup/**")
+                .addResourceLocations("file:///" + System.getProperties().getProperty("user.home") + "/halo/backup/");
+    }
 }
