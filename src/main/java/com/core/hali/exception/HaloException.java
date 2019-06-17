987529a849e93d011e78495defef21a4aa55bd1f
zhouzhisheng <17666545369@163.com>
2019-06-17 23:57:02 +0800

    定义异常

--- src/main/java/com/core/hali/exception/HaloException.java
+++ src/main/java/com/core/hali/exception/HaloException.java
@@ -1,5 +1,10 @@
 package com.core.hali.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import cc.ryanc.halo.exception.HaloException;
 
 /**
  * 
@@ -13,4 +18,32 @@ public final class HaloException  extends HaloException{
 	 * Error errorDate
 	 */
 	private Object errorDate;
+	
+	public HaloException(String message) {
+        super(message);
+    }
+
+    public HaloException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @NonNull
+    public abstract HttpStatus getStatus();
+
+    @Nullable
+    public Object getErrorData() {
+        return errorData;
+    }
+
+    /**
+     * Sets error errorData.
+     *
+     * @param errorData error data
+     * @return current exception.
+     */
+    @NonNull
+    public HaloException setErrorData(@Nullable Object errorData) {
+        this.errorData = errorData;
+        return this;
+    }
 }
